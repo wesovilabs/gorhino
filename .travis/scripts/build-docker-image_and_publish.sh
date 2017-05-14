406f598bad67a89a7ec17b1aa543bbd4d87204b3
ivan.corrales <ivan.corrales@bbva.com>
2017-05-14 07:09:34 +0200

    feat: adding dockerhub ci

--- .travis/scripts/build-docker-image_and_publish.sh
+++ .travis/scripts/build-docker-image_and_publish.sh
@@ -16,23 +16,28 @@ publishDockerImage(){
 
 
 
-if [ "$TRAVIS_BRANCH" == /^master/ ]
+if [[ "$TRAVIS_BRANCH" == /^master$/ ]]
 then
     export TAG="latest";
     buildDockerImage
     publishDockerImage
-else
-    if [ "$TRAVIS_BRANCH" == /^develop$/ ]
-    then
-        export TAG="develop";
-        buildDockerImage
-        publishDockerImage
-    else
-        export TAG="feature";
-        buildDockerImage
-        publishDockerImage
-    fi
 fi
+
+
+if [[ "$TRAVIS_BRANCH" == /^develop$/ ]]
+then
+    export TAG="develop";
+    buildDockerImage
+    publishDockerImage
+fi
+
+if [[ "$TRAVIS_BRANCH" == /^feature/.*$/ ]]
+then
+    export TAG="develop";
+    buildDockerImage
+    publishDockerImage
+fi
+
 
 
 
