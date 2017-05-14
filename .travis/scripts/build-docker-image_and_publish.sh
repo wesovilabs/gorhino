9c4ea01442047960414288d8c3b8cc4653425dbf
ivan.corrales <ivan.corrales@bbva.com>
2017-05-14 07:04:04 +0200

    feat: adding dockerhub ci

--- .travis/scripts/build-docker-image_and_publish.sh
+++ .travis/scripts/build-docker-image_and_publish.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 
-if [ "$TRAVIS_BRANCH" == /^master/ ]; then
+
+buildDockerImage(){
+    docker build -t $REPO:$TAG -f Dockerfile .
+    return 0
+}
+
+publishDockerImage(){
+    docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+    docker tag $REPO:$COMMIT $REPO:$TAG
+    docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+    docker push $REPO
+    return 0
+}
+
+
+
+if [ "$TRAVIS_BRANCH" == /^master/ ]
+then
     export TAG="latest";
-else if [ "$TRAVIS_BRANCH" == /^develop$/ ]; then
-    export TAG="develop";
 else
-    export TAG="feature";
+    if [ "$TRAVIS_BRANCH" == /^develop$/ ]
+    then
+        export TAG="develop";
+    else
+        export TAG="feature";
+    fi
 fi
 
-docker build -t $REPO:$TAG -f Dockerfile .
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
+
+
+
